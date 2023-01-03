f72c1cc2340c3be5567c0b6f9a76a0307e71de1e
ElidaSeptriani <113493736+ElidaSeptriani@users.noreply.github.com>
2023-01-03 23:56:02 +0700

    Add files via upload

---
+++ pengembalian-service/src/main/java/com/eli/pengembalian/service/VO/ResponseTemplateVO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.eli.pengembalian.service.VO;
+
+import com.eli.pengembalian.service.entity.Pengembalian;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Acer
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+    private Pengembalian pengembalian;
+    private Peminjaman peminjaman;
+}
